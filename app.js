38a45687a4b728b1c6c75a42f42133812fd0883d
youngcourtcourt <courtneymacdonald@Courtneys-iMac.local>
2021-05-01 09:21:51 -0700

    Added axes to chart

--- app.js
+++ app.js
@@ -1,4 +1,26 @@
 // @TODO: YOUR CODE HERE!
+var svgWidth=960
+var svgHeight=500
+
+var margin={
+    top:60,
+    bottom:60, 
+    right:60, 
+    left:60
+}
+
+var chartWidth=svgWidth-margin.left-margin.right
+var chartHeight=svgHeight-margin.top-margin.bottom
+
+var svg=d3.select("#scatter")
+        .append("svg")
+        .attr("width", svgWidth)
+        .attr("height", svgHeight)
+
+var chartGroup=svg.append("g")
+                .attr("transform", `translate(${margin.left}, ${margin.top})`)
+
+
 
 d3.csv("data.csv").then(function(data){
     console.log(data)
@@ -21,4 +43,19 @@ d3.csv("data.csv").then(function(data){
         d.smokesLow= +d.smokesLow
         d.smokesHigh= +d.smokesHigh
     })
+
+var xLinearScale=d3.scaleLinear()
+            .domain(d3.extent(data, d=>d.poverty))
+            .range([0, chartWidth])
+var yLinearScale=d3.scaleLinear()
+    .domain(d3.extent(data, d=>d.smokes))
+    .range([chartHeight, 0])
+
+var bottomAxis=d3.axisBottom(xLinearScale)
+
+var leftAxis=d3.axisLeft(yLinearScale)
+
+chartGroup.append("g").attr("transform", `translate(0, ${chartHeight})`).call(bottomAxis)
+chartGroup.append("g").call(leftAxis)
+
 })
