6d520ad3caa78dc6aa2d98c78d89bd6ea3ab52d5
youngcourtcourt <courtneymacdonald@Courtneys-iMac.local>
2021-04-30 17:05:55 -0700

    Added forEach loop

--- app.js
+++ app.js
@@ -2,4 +2,23 @@
 
 d3.csv("data.csv").then(function(data){
     console.log(data)
+
+    data.forEach(d=>{
+
+        d.poverty= +d.poverty
+        d.povertyMoe= +d.povertyMoe
+        d.age= +d.age
+        d.ageMoe= +d.ageMoe
+        d.income= +d.income
+        d.incomeMoe= +d.incomeMoe
+        d.healthcare= +d.healthcare
+        d.healthcareLow= +d.healthcareLow
+        d.heatlhcareHigh= +d.heatlhcareHigh
+        d.obesity= +d.obesity
+        d.obesityLow= +d.obesityLow
+        d.obesityHigh= +d.obesityHigh
+        d.smokes= +d.smokes
+        d.smokesLow= +d.smokesLow
+        d.smokesHigh= +d.smokesHigh
+    })
 })
